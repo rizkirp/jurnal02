cdbb98196d4d276ace701c050baeccb60e303456
rizkirp <91263116+rizkirp@users.noreply.github.com>
2021-10-02 12:07:54 +0700

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+
+    public static void main(String[] args) {
+	    System.out.println("Nama:Rizki Rahmadia");
+		System.out.println("Nim:6706210146");
+		System.out.println("Kelas: 45-05");
+	}
+}
